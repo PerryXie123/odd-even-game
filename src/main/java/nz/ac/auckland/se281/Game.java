62eec3e2be596e92e4628639203991515513a620
Perry Xie <perryxie.nz@gmail.com>
2024-05-04 14:23:02 +1200

    Added a message to ask for user input, and prints a message if it is valid.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,15 +6,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   public int gameNumber = 0;
+  public String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     gameNumber++;
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-    MessageCli.ASK_INPUT.printMessage();
+    playerName = options[0];
   }
 
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(gameNumber));
+    MessageCli.ASK_INPUT.printMessage();
+    String input = Utils.scanner.nextLine();
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, input);
   }
 
   public void endGame() {}
