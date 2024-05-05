89d80c60551d441fd119e28bea2dc189bbf29dc2
Perry Xie <perryxie.nz@gmail.com>
2024-05-06 00:14:34 +1200

    Modified the Hard strategy to use the correct round counter.

--- src/main/java/nz/ac/auckland/se281/Hard.java
+++ src/main/java/nz/ac/auckland/se281/Hard.java
@@ -1,13 +1,12 @@
 package nz.ac.auckland.se281;
 
 public class Hard implements Strategy {
-  private int roundCount = 0;
   private Strategy currentStrategy = new Random();
   private int lastWinStrategy = 0;
 
   @Override
   public int chooseFinger(int round, int oddCount, int evenCount, String choiceString) {
-    if (roundCount > 3) {
+    if (round > 3) {
       if (lastWinStrategy == 0) {
         currentStrategy = new Top();
         lastWinStrategy = 1;
