1caeb5218f906830ea8f8f9f24aa8371cd6056a9
Perry Xie <perryxie.nz@gmail.com>
2024-05-05 22:52:31 +1200

    Fixed round counting variable in the Medium class.

--- src/main/java/nz/ac/auckland/se281/Medium.java
+++ src/main/java/nz/ac/auckland/se281/Medium.java
@@ -1,13 +1,14 @@
 package nz.ac.auckland.se281;
 
 public class Medium implements Strategy {
-  private int roundCount = 0;
   private Strategy currentStrategy = new Random();
 
   @Override
   public int chooseFinger(int round, int oddCount, int evenCount, String choiceString) {
-    if (roundCount > 3) {
+    if (round > 3) {
       currentStrategy = new Top();
+    } else {
+      currentStrategy = new Random();
     }
     return currentStrategy.chooseFinger(round, oddCount, evenCount, choiceString);
   }
